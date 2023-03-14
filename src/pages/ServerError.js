a43927f9f28e426bb73391f5930b986b91c5cfc8
Nghi Vo <84538758+nghigaxx@users.noreply.github.com>
2023-03-13 21:34:44 -0400

    Create ServerError.js

---
+++ src/pages/ServerError.js
@@ -0,0 +1,11 @@
+function ServerError() {
+    return (
+      <div>
+        <h1>Application Failed</h1>
+        <p>Sorry, we are having technical difficulties on our side. Please try again later.</p>
+        <p>Thank you for your understanding</p>
+      </div>
+    );
+  }
+  
+  export default ServerError;
